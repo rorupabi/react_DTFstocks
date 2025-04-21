24442674b667991abdbc8e71182cf0ed663609aa
Ro Padilla <157687882+rorupabi@users.noreply.github.com>
2025-04-21 12:00:05 +0800

    pr

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from 'react'
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
 
@@ -23,59 +22,17 @@ const Navbar = () => {
         {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
 
-      <div className={!nav ? 'fixed lg:hidden left-0 top-0 w-[30%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
+      <div className={!nav ? 'fixed lg:hidde md:hidden left-0 top-0 w-[30%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
         <h1 className=' w-full text-3xl font-bold text-[#00df9a] m-4'>DtF.</h1>
         <ul className=' uppercase p-4'>
           <li className=' p-4 border-b border-gray-600 cursor-pointer text-white hover:text-gray-800 font-semibold'>Home</li>
           <li className=' p-4 border-b border-gray-600 cursor-pointer text-white hover:text-gray-800 font-semibold'>Company</li>
           <li className=' p-4 border-b border-gray-600 cursor-pointer text-white hover:text-gray-800 font-semibold'>Resources</li>
           <li className=' p-4 border-b border-gray-600 cursor-pointer text-white hover:text-gray-800 font-semibold'>About</li>
-          <li className=' p-4 '>Contact</li>
+          <li className=' p-4 border-b border-gray-600 cursor-pointer text-white hover:text-gray-800 font-semibold'>Contact</li>
         </ul>
       </div>
     </div>
   );
 };
 export default Navbar
-=======
-import React, { useState } from 'react'
-import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
-
-const Navbar = () => {
-  const [nav, setNav] = useState(false)
-
-  const handleNav = () => {
-    setNav(!nav)
-  }
-
-  return (
-    <div className='flex justify-between items-center max-w-[1240px] mx-auto px-4 h-24 text-white'>
-      <h1 className=' w-full text-3xl font-bold text-[#00df9a]'>DtF.</h1>
-      <ul className='hidden md:flex'>
-        <li className='p-4'>Home</li>
-        <li className='p-4'>Company</li>
-        <li className='p-4'>Resources</li>
-        <li className='p-4'>About</li>
-        <li className='p-4'>Contact</li>
-      </ul>
-      <div onClick={handleNav} className='block md:hidden cursor-pointer'>
-        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
-      </div>
-
-      {/* Mobile menu */}
-      <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%] ease-in-out duration-500'}>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>DtF.</h1>
-        <ul className='uppercase p-4'>
-          <li className='p-4 border-b border-gray-600'>Home</li>
-          <li className='p-4 border-b border-gray-600'>Company</li>
-          <li className='p-4 border-b border-gray-600'>Resources</li>
-          <li className='p-4 border-b border-gray-600'>About</li>
-          <li className='p-4'>Contact</li>
-        </ul>
-      </div>
-    </div>
-  );
-};
-
-export default Navbar;
->>>>>>> 11258b334624bf403a2a0172581b4d96886aa77e
